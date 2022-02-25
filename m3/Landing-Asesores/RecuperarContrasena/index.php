6a10b4e3c0a4979a4e9ef13b934212b47f4dbf8b
marcela.vaquiro <yojann96@hotmail.com>
2022-02-25 17:33:58 -0500

    Landing-asesores: Servicio Recuperar contrasena finalizado

---
+++ m3/Landing-Asesores/RecuperarContrasena/index.php
@@ -0,0 +1,75 @@
+<?php
+require __DIR__ . '/../../Core/vendor/autoload.php';
+require_once __DIR__ . '/../../Core/config.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+// Se crea la app
+$app = new \Slim\App();
+$container = $app->getContainer();
+$container['view'] = new \Slim\Views\PhpRenderer(__DIR__.'/template/');
+
+$container['curlWigi'] = new \wigilabs\curlWigiM3\curlWigi();
+
+//Url del servicio
+$container['urlServicio']="http://172.24.160.135:8080/EXP_RSCustomerDataOtt/PS_RSCustomerDataOttV1.0";    //Producción
+
+//Nombre del template Request
+$container['requestTemplate']="request.php"; 
+
+$app->map(['POST'], '/', function (Request $request, Response $response, array $args) {
+    
+    //$headers = $request->getHeaders();
+    $json = json_decode( $request->getBody() );
+    $data=$json->data;
+    
+    $reqXML = $this->view->fetch($this->requestTemplate, ['data' => $data]);
+
+    $this->curlWigi->URL=$this->urlServicio;
+    $this->curlWigi->POSTFIELDS=$reqXML;
+    
+    $header[]="";
+    
+    $dataRes=$this->curlWigi->soap($header,true,true);
+
+    /************************LOGICA DE RESPONSE***************************/
+    
+    $respuesta = array();
+    if($dataRes["error"] == 0){
+        //print_r($dataRes);die;
+        //Tag que envia el servicio
+        $tagResp = "Root-Element";
+
+        $respuesta["secs"] = $dataRes["secs"];
+        $respuesta["tiempo"] = $dataRes["tiempo"];
+        if(isset($dataRes["response"],$dataRes["response"]->$tagResp)){
+            $dataRes = $dataRes["response"]->$tagResp->updateUserOttResponse;
+            if( strval($dataRes->resultCode) == 0){
+                $respuesta["response"] = strval($dataRes->resultMessage);
+                $respuesta["error"] = 0;                
+            }else{
+                $respuesta["error"] = 1;
+                $respuesta["response"] = strval($dataRes->resultMessage);
+            }
+        }else{
+            $respuesta["error"] = 1;
+            $respuesta["response"] = strval($dataRes['responseServer']);
+        }
+    }else {
+        $respuesta["error"] = 1;
+        $respuesta["response"] = $dataRes["responseServer"];
+    }
+    
+    //var_dump($respuesta);die;
+    return $response->withJson($respuesta)->withHeader('Content-type', 'application/json'); 
+    
+
+});
+
+
+$app->run();
