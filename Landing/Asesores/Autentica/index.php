1eede68a02a3128850b819c0e0eafd47fc76f471
Marcela Vaquiro <marcela.vaquiro@novatec.com.co>
2022-03-09 11:34:23 -0500

    Ajuste de CORS

--- Landing/Asesores/Autentica/index.php
+++ Landing/Asesores/Autentica/index.php
@@ -1,11 +1,12 @@
 <?php
-require __DIR__ . '/../../Core/vendor/autoload.php';
-require_once __DIR__ . '/../../Core/utils/CurlClass.php';
-require __DIR__ . '/../../Core/Middleware.php';
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
+require __DIR__ . '/../../Core/vendor/autoload.php';
+require_once __DIR__ . '/../../Core/utils/CurlClass.php';
+require __DIR__ . '/../../Core/Middleware.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
