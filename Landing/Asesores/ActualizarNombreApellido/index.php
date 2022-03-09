12c6274bbbee58691f6d6e9babe99bccb776dc45
marcela.vaquiro <yojann96@hotmail.com>
2022-03-09 12:07:32 -0500

    blindaje servicios correo, linea movil, cambio nombre-apellido

--- Landing/Asesores/ActualizarNombreApellido/index.php
+++ Landing/Asesores/ActualizarNombreApellido/index.php
@@ -1,15 +1,12 @@
 <?php
-require __DIR__ . '/../../Core/vendor/autoload.php';
-require_once __DIR__ . '/../../Core/utils/CurlClass.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+require __DIR__ . '/../../Core/vendor/autoload.php';
+require __DIR__ . '/../../Core/Middleware.php';
+require_once __DIR__ . '/../../Core/utils/CurlClass.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// Se crea la app
 $app = new \Slim\App();
 $container = $app->getContainer();
 $container['view'] = new \Slim\Views\PhpRenderer(__DIR__.'/template/');
@@ -21,52 +18,56 @@ $container['curlClass'] = new CurlClass();
 //$container['urlServicio']="http://172.24.160.161:8600/EXP_WSCustomeCusID/PS_WSCustomeCusIDV1.0";    //Pre Produccion
 $container['urlServicio']="http://172.24.160.135:8080/EXP_WSCustomeCusID/PS_WSCustomeCusIDV1.0";    //Produccion
 
-//Nombre del template Request
 $container['requestTemplate']="request.php"; 
+
+$app->add(new MiddlewareApp(dirname(__FILE__), $app->getContainer()));
 
 $app->map(['POST'], '/', function (Request $request, Response $response, array $args) {
     
-    //$headers = $request->getHeaders();
-    $json = json_decode( $request->getBody() );
-    $data=$json->data;
+    $dataJson = $request->getAttribute('dataJson');
     
-    $reqXML = $this->view->fetch($this->requestTemplate, ['data' => $data]);
+    if( isset($dataJson->customerId) && isset($dataJson->providerId) ){
+    
+        $reqXML = $this->view->fetch($this->requestTemplate, ['data' => $dataJson]);
 
-    $this->curlClass->URL=$this->urlServicio;
-    $this->curlClass->POSTFIELDS=$reqXML;
-    
-    $header[]="";
-    
-    $dataRes=$this->curlClass->soap($header,true,true);
-    
-    /************************LOGICA DE RESPONSE***************************/
-    
-    $respuesta = array();
-    if($dataRes["error"] == 0){
+        $this->curlClass->URL=$this->urlServicio;
+        $this->curlClass->POSTFIELDS=$reqXML;
         
-        $tagResp = "ns2rootModifyResponse";
+        $header[]="";
+        
+        $dataRes=$this->curlClass->soap($header,true,true);
+        
+        /************************LOGICA DE RESPONSE***************************/
+        
+        $respuesta = array();
+        if($dataRes["error"] == 0){
+            
+            $tagResp = "ns2rootModifyResponse";
 
-        $respuesta["secs"] = $dataRes["secs"];
-        $respuesta["tiempo"] = $dataRes["tiempo"];
+            $respuesta["secs"] = $dataRes["secs"];
+            $respuesta["tiempo"] = $dataRes["tiempo"];
 
-        if(isset($dataRes["response"],$dataRes["response"]->$tagResp)){
-            $dataRes = $dataRes["response"]->$tagResp->ns2modifyResponse;
-            if( $dataRes->ns2resultCode == 0){
-                $respuesta["error"] = $dataRes->ns2resultCode;
-                $respuesta["response"] = $dataRes->ns2resultMessage;
+            if(isset($dataRes["response"],$dataRes["response"]->$tagResp)){
+                $dataRes = $dataRes["response"]->$tagResp->ns2modifyResponse;
+                if( $dataRes->ns2resultCode == 0){
+                    $respuesta["error"] = $dataRes->ns2resultCode;
+                    $respuesta["response"] = $dataRes->ns2resultMessage;
+                }else{
+                    $respuesta["error"] = 1;
+                    $respuesta["response"] = "No es posible acceder por el momento";
+                }
             }else{
                 $respuesta["error"] = 1;
-                $respuesta["response"] = "No es posible acceder por el momento";
+                $respuesta["response"] = "Error al consultar";
             }
-        }else{
+        }else {
             $respuesta["error"] = 1;
-            $respuesta["response"] = "Error al consultar";
+            $respuesta["response"] = $dataRes["response"];
         }
     }else {
         $respuesta["error"] = 1;
-        $respuesta["response"] = $dataRes["response"];
+        $respuesta["response"] = "Error en data. Favor validar";
     }
-    
     return $response->withJson($respuesta)->withHeader('Content-type', 'application/json'); 
     
 
--- Landing/Asesores/LineaMovil/index.php
+++ Landing/Asesores/LineaMovil/index.php
@@ -1,98 +1,95 @@
 <?php
-require __DIR__ . '/../../Core/vendor/autoload.php';
-require_once __DIR__ . '/../../Core/utils/CurlClass.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+require __DIR__ . '/../../Core/vendor/autoload.php';
+require __DIR__ . '/../../Core/Middleware.php';
+require_once __DIR__ . '/../../Core/utils/CurlClass.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// Se crea la app
 $app = new \Slim\App();
 $container = $app->getContainer();
 $container['view'] = new \Slim\Views\PhpRenderer(__DIR__.'/template/');
 
 $container['curlClass'] = new CurlClass();
 
-//Url del servicio
 //$container['urlServicio']="http://172.24.160.161:8600/EXP_WSCustomeCusID/PS_WSCustomeCusIDV1.0";    //Desarrollo
 //$container['urlServicio']="http://172.24.160.161:8600/EXP_WSCustomeCusID/PS_WSCustomeCusIDV1.0";    //Pre Produccion
 $container['urlServicio']="http://172.24.160.135:8080/EXP_WSCustomeCusID/PS_WSCustomeCusIDV1.0";    //Produccion
-
-//Nombre del template Request
 $container['requestTemplate']="request.php"; 
 
-
+$app->add(new MiddlewareApp(dirname(__FILE__), $app->getContainer()));
 
 $app->map(['POST'], '/', function (Request $request, Response $response, array $args) {
     
-    //$headers = $request->getHeaders();
-    $json = json_decode( $request->getBody() );
-    $data=$json->data;
+    $dataJson = $request->getAttribute('dataJson');
+    
+    if( isset($dataJson->msisdn) && isset($dataJson->state) ){
 
-    $reqXML = $this->view->fetch($this->requestTemplate, ['data' => $data]);
-    
-    $this->curlClass->URL=$this->urlServicio;
-    $this->curlClass->POSTFIELDS=$reqXML;
-    
-    $header[]="";
-    
-    $dataRes=$this->curlClass->soap($header,true,true);
-    
-    /************************LOGICA DE RESPONSE***************************/
-    
-    $respuesta = array();
-    if($dataRes["error"] == 0){
+        $reqXML = $this->view->fetch($this->requestTemplate, ['data' => $dataJson]);
         
-        //Tag que envia el servicio
-        $tagResp = "ns2rootReadResponse";
-
-        $respuesta["secs"] = $dataRes["secs"];
-        $respuesta["tiempo"] = $dataRes["tiempo"];
+        $this->curlClass->URL=$this->urlServicio;
+        $this->curlClass->POSTFIELDS=$reqXML;
         
-        if(isset($dataRes["response"],$dataRes["response"]->$tagResp)){
-            $dataRes = $dataRes["response"]->$tagResp;
+        $header[]="";
+        
+        $dataRes=$this->curlClass->soap($header,true,true);
+        
+        /************************LOGICA DE RESPONSE***************************/
+        
+        $respuesta = array();
+        if($dataRes["error"] == 0){
             
-            if( strval($dataRes->ns2readResponse->ns2resultCode) == 0){
-                $dataRes = $dataRes->ns2readResponse->ns2data;
+            //Tag que envia el servicio
+            $tagResp = "ns2rootReadResponse";
+
+            $respuesta["secs"] = $dataRes["secs"];
+            $respuesta["tiempo"] = $dataRes["tiempo"];
+            
+            if(isset($dataRes["response"],$dataRes["response"]->$tagResp)){
+                $dataRes = $dataRes["response"]->$tagResp;
                 
-                $customerId = strval($dataRes->ns2customerId);
-                $providerId = strval($dataRes->ns2providerId);
-                $idNumber = strval($dataRes->ns2idNumber);
-                $fixedAccount = strval($dataRes->ns2fixedAccount);
-                $operatorUserId = strval($dataRes->ns2operatorUserId);
-                $emailAddress = strval($dataRes->ns2emailAddress);
-                $firstName = strval($dataRes->ns2firstName);
-                $lastName = strval($dataRes->ns2lastName);
-                $paymentType = strval($dataRes->ns2paymentType);
-                $msisdn = strval($dataRes->ns2msisdn);
-                $arrayResponse = [
-                    'customerId'=>$customerId,
-                    'providerId'=> $providerId,
-                    'idNumber'=> $idNumber,
-                    'fixedAccount'=> $msisdn,
-                    'operatorUserId'=> $operatorUserId,
-                    'emailAddress'=> $emailAddress,
-                    'firstName'=> $firstName,
-                    'lastName'=> $lastName,
-                    'paymentType'=> $paymentType,
-                    'msisdn' => $msisdn
-                ];
-                $respuesta["response"] = $arrayResponse;
-                $respuesta["error"] = 0;
-            }else{
-                $respuesta["error"] = 1;
-                $respuesta["response"] = strval($dataRes->ns2readResponse->ns2resultMessage);
+                if( strval($dataRes->ns2readResponse->ns2resultCode) == 0){
+                    $dataRes = $dataRes->ns2readResponse->ns2data;
+                    
+                    $customerId = strval($dataRes->ns2customerId);
+                    $providerId = strval($dataRes->ns2providerId);
+                    $idNumber = strval($dataRes->ns2idNumber);
+                    $fixedAccount = strval($dataRes->ns2fixedAccount);
+                    $operatorUserId = strval($dataRes->ns2operatorUserId);
+                    $emailAddress = strval($dataRes->ns2emailAddress);
+                    $firstName = strval($dataRes->ns2firstName);
+                    $lastName = strval($dataRes->ns2lastName);
+                    $paymentType = strval($dataRes->ns2paymentType);
+                    $msisdn = strval($dataRes->ns2msisdn);
+                    $arrayResponse = [
+                        'customerId'=>$customerId,
+                        'providerId'=> $providerId,
+                        'idNumber'=> $idNumber,
+                        'fixedAccount'=> $msisdn,
+                        'operatorUserId'=> $operatorUserId,
+                        'emailAddress'=> $emailAddress,
+                        'firstName'=> $firstName,
+                        'lastName'=> $lastName,
+                        'paymentType'=> $paymentType,
+                        'msisdn' => $msisdn
+                    ];
+                    $respuesta["response"] = $arrayResponse;
+                    $respuesta["error"] = 0;
+                }else{
+                    $respuesta["error"] = 1;
+                    $respuesta["response"] = strval($dataRes->ns2readResponse->ns2resultMessage);
+                }
             }
+        }else {
+            $respuesta["error"] = 1;
+            $respuesta["response"] = $dataRes["response"];
         }
     }else {
         $respuesta["error"] = 1;
-        $respuesta["response"] = $dataRes["response"];
+        $respuesta["response"] = "Error en data. Favor validar";
     }
     
-    //var_dump($respuesta);die;
     return $response->withJson($respuesta)->withHeader('Content-type', 'application/json'); 
     
 
