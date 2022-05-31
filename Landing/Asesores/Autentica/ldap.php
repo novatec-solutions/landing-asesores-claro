92ad2178584bea0879475c3277f86766971ab8bd
Herman Andres <heanfig@gmail.com>
2022-05-31 11:19:39 -0500

    Ajuste de inicio de sesion de LDAP

---
+++ Landing/Asesores/Autentica/ldap.php
@@ -0,0 +1,22 @@
+<?php
+
+    $ldap = [
+        'timeout' => 20,
+        'host' => '172.24.232.140',
+        'rdn' => 'CLAROCO\\' . 'ICF6344A',
+        'pass' => 'Samantha2022.'
+    ];
+    $host = $ldap["host"];
+    $ldapport = 389;
+
+    $ldapconn = ldap_connect($host, $ldapport)  or die("Fallo conexion con LDPA");
+
+    ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
+
+    if ($ldapconn) {
+        /* Realiza la autenticacion */
+        $ldapbind = ldap_bind($ldapconn, $ldap["rdn"], $ldap["pass"]);
+        
+        var_dump($ldapbind);
+    }
